866cb18a17eba749f402c85dc888a19f2ddb7f7d
jose Angel <josegallardo.cor@gmail.com>
2025-04-19 23:45:58 -0600

    primer commit

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿
+
+using TaskManagerAppConsole;
+
+TaskManager gestor = new TaskManager();
+string opcion = "";
+
+do {
+    Console.WriteLine("\n-- Gestion de tareas ---");
+    Console.WriteLine("1. Agregar tarea");
+    Console.WriteLine("2. Completar tarea");
+    Console.WriteLine("3. Ver tareas pendientes");
+    Console.WriteLine("4. Ver historial");
+    Console.WriteLine("0. Salir");
+    Console.Write("Elige la opcion: ");
+    opcion = Console.ReadLine();
+    switch (opcion)
+    {
+        case "1":
+            Console.WriteLine("Descripcion de la tarea: ");
+            string descripcion = Console.ReadLine();
+            gestor.AgregarTarea(descripcion);
+            break;
+        case "2":
+            gestor.CompletarTarea();
+            break;
+        case "3":
+            gestor.verTareas();
+            break;
+        case "4":
+            gestor.verHistorial();
+            break;
+    }
+}  while (opcion != "0");
---
+++ Tarea.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TaskManagerAppConsole
+{
+    public class Tarea
+    {
+        public string Descripcion { get; set; }
+        public DateTime FechaCreacion { get; set; }
+        public Tarea(string decripcion)
+        {
+            Descripcion = decripcion;
+            FechaCreacion = DateTime.Now;   
+        }
+        public override string ToString()
+        {
+            return $"{Descripcion} (Creada el {FechaCreacion})";
+        }
+    }
+}
